d4dcfe0194a6af79b76d385601523e45c6aa5433
khriztianmoreno <khriztianmoreno@gmail.com>
2019-06-14 22:24:27 -0500

    Add the middleware to user endpoint

--- api/user/index.js
+++ api/user/index.js
@@ -6,8 +6,14 @@
 const { Router } = require('express');
 const controller = require('./user.controller');
 
+// New line
+const auth = require('./../../auth/auth.service');
+
+
 const router = new Router();
 
+// New Line
+router.get('/', auth.isAuthenticated(), controller.index);
 router.post('/', controller.create);
 
 module.exports = router;
